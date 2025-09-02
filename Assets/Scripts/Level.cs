15c9685aa09ddf5e3032cc24c2d85b662aa0a182
Lucas <tompouts@gmail.com>
2025-09-03 01:12:03 +0200

    Added NaughtyAttributes & Level logic mostly complete

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -1,18 +1,33 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NaughtyAttributes;
+using TMPro;
 
 public class Level : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private string _levelName;
+    public string LevelName => _levelName;
+    
+    [SerializeField] private int _par;
+    public int Par => _par;
+    
+    [SerializeField] private List<SpriteRenderer> _wallSpriteRenderers = new List<SpriteRenderer>();
 
-    // Update is called once per frame
-    void Update()
+    [Button("Collect Wall SpriteRenderers")]
+    public void CollectWallSpriteRenderers()
     {
-        
+        _wallSpriteRenderers.Clear(); // Clear the list to avoid duplicates
+
+        foreach (Transform child in transform)
+        {
+            if (child.name == "Wall")
+            {
+                SpriteRenderer spriteRenderer = child.GetComponent<SpriteRenderer>();
+                if (spriteRenderer != null)
+                {
+                    _wallSpriteRenderers.Add(spriteRenderer);
+                }
+            }
+        }
     }
 }
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using NaughtyAttributes;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class LevelManager : MonoBehaviour
 {
     private static LevelManager _instance;
     public static LevelManager Instance { get { return _instance; } }
     
-    [SerializeField] private List<Level> _levels;
+    [SerializeField] private List<GameObject> _levelPrefabs;
     private int _currentLevelIndex = 0;
-    public Level CurrentLevel => _levels[_currentLevelIndex];
+    public GameObject CurrentLevelObject => _levelPrefabs[_currentLevelIndex];
     
     private void Awake()
     {  
@@ -19,5 +21,20 @@ public class LevelManager : MonoBehaviour
     public void EndLevel()
     {
         Debug.Log("Ending level");
+    }
+    
+    [Button("Load Next Level")]
+    public void LoadNextLevel()
+    {
+        if (_currentLevelIndex <= _levelPrefabs.Count - 1)
+        {
+            Debug.Log("Loading level: " + _levelPrefabs[_currentLevelIndex].name);
+            GameObject newLevelObject = Instantiate(_levelPrefabs[_currentLevelIndex], Vector2.zero, Quaternion.identity);
+            _currentLevelIndex++;
+        }
+        else
+        {
+            Debug.Log("No more levels to load.");
+        }
     }
 }
