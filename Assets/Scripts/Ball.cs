e361039097519753bc9f55a9b39ac30fdb977f0a
Lucas <tompouts@gmail.com>
2025-09-02 21:19:59 +0200

    Golfball bounces fixed

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,49 +2,68 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    [SerializeField] private float _speed = 10f;
+    [SerializeField] private float _speed = 5f;
     private Rigidbody2D _rb2D;
+    private LineRenderer _lineRenderer;
 
     private bool _isDragging;
 
     void Start()
     {
         _rb2D = GetComponent<Rigidbody2D>();
-    }   
-    
+        _lineRenderer = GetComponent<LineRenderer>();
+        _lineRenderer.enabled = false;
+    }
 
     void Launch(Vector2 force)
     {
         _rb2D.velocity = force * _speed;
     }
-    
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            StartDrag();
+            CheckForBallClick();
         }
-        
-        if (Input.GetKeyUp(KeyCode.Mouse0))
+
+        if (Input.GetKeyUp(KeyCode.Mouse0) && _isDragging)
         {
             EndDrag();
         }
 
         if (!_isDragging) return;
-        
+
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Debug.DrawLine(transform.position, mousePos, Color.red);
+        mousePos.z = 0;
+        _lineRenderer.SetPosition(0, transform.position);
+        _lineRenderer.SetPosition(1, mousePos);
+    }
+
+    private void CheckForBallClick()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+
+        RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
+        if (hit.collider != null && hit.collider.gameObject == gameObject)
+        {
+            StartDrag();
+        }
     }
 
     private void StartDrag()
     {
         _isDragging = true;
+        _lineRenderer.enabled = true;
     }
-    
+
     private void EndDrag()
     {
         _isDragging = false;
+        _lineRenderer.enabled = false;
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
         Vector2 direction = -(mousePos - transform.position);
         float distance = direction.magnitude;
         Launch(direction.normalized * distance);
